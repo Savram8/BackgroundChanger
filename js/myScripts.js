2630ffde032aacbbfb2e20966fe6ec26cc7bfcf1
Stefan Avram <Savram@stagewood.com>
2020-09-04 13:40:30 -0400

    finished project

--- js/myScripts.js
+++ js/myScripts.js
@@ -2,20 +2,28 @@
 
 //creating a variable that will get the background document
 var background = document.getElementById("Background")
+//button variable holding the button
 var button = document.getElementById("button")
 
-var colors = [];
-while (colors.length < 100) {
-    do {
-        var color = Math.floor((Math.random()*1000000)+1);
-    } while (colors.indexOf(color) >= 0);
-    colors.push("#" + ("000000" + color.toString(16)).slice(-6));
-}
+
+//Array of colors
+var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+   
+      
 
 
 
-
-function changeColor(newColor) {
-  var elem = document.getElementById('Background');
-  elem.style.color = newColor;
-}
+    button.onclick = function(){
+    var randomItem = colorArray[Math.floor(Math.random()*colorArray.length)];
+    document.body.style.backgroundColor = randomItem; 
+  
+};
